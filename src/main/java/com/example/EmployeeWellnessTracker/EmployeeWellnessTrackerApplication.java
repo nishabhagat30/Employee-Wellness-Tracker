b527081bcc99097c2d5dfde8876108d44c959d55
nishabhagat30 <nishabhagat573@gmail.com>
2025-03-26 19:40:37 +0530

    Initial Commit

---
+++ src/main/java/com/example/EmployeeWellnessTracker/EmployeeWellnessTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeWellnessTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWellnessTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWellnessTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EmployeeWellnessTracker/EmployeeWellnessTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeWellnessTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWellnessTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
